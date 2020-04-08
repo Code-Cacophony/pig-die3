e516c01922c305931849972272ea1e213e855b76
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-04-08 14:17:17 -0700

    add ids to players

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,59 @@
 function Player(name) {
   this.name = name,
     this.turnScore = 0,
-    this.totalScore = 0,
+    this.totalScore = 0
 }
 
-
-
-
+Game.prototype.updateTurnScore = function (rollNumber, turn) {
+  var id = turn;
+  for (var i = 0; i < this.players.length; i++) {
+    if (this.players[i]) {
+      if (this.players[i].id == id) {
+        return this.players[i];
+      }
+    }
+  };
+  return false
+}
 
 
 //game logic
 function Game(player1, player2) {
-  this.player1 = player1,
-    this.player2 = player2,
+  this.players = [];
+  this.currentId = 0,
     this.playerTurn = 1,
     this.gameOver = false
 }
 
-Game.prototype.executeTurn = function () {
-  // var rollNumber = game.roll();
+Game.prototype.addPlayer = function (player) {
+  player.id = this.assignId();
+  this.players.push(player);
+}
+
+Game.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Game.prototype.executeTurn = function (roll, turn) {
+  var rollNumber = roll;
+  var pTurn = turn;
+
+  this.updateTurnScore(rollNumber, pTurn);
+
+  // if (pTurn === 1) {
+  //   game.updateTurnScore(rollNumber, pTurn);
+  //   console.log(update.turnScore);
+  // } else {
+  //   player2.turnScore += rollNumber;
+  //   console.log(player2.turnScore);
+  // }
+
+  //console.log(rollNumber);
+  $("#rollDisplay").text(rollNumber);
+
+  //return rollNumber;
+
   // if rollNumber === 1
   //      game.endTurn
   // else
@@ -29,8 +64,9 @@ Game.prototype.executeTurn = function () {
 }
 
 Game.prototype.roll = function () {
-  // return a random number, 1-6
-  // 
+  var number = Math.floor(Math.random() * 6) + 1;
+  //console.log(number);
+  return number;
 
 }
 
@@ -68,16 +104,23 @@ $(document).ready(function () {
 
     var pOne = new Player(pOneName);
     var pTwo = new Player(pTwoName);
+    var game = new Game();
 
-    var game = new Game(pOne, pTwo);
+    game.addPlayer(pOne);
+    game.addPlayer(pTwo);
+
+    $("#playerOneNameDisplay").text(pOne.name);
+    $("#playerTwoNameDisplay").text(pTwo.name);
+    $("#turnDisplay").text(pOne.name);
 
     console.log(pOne.name);
     console.log(pTwo.name);
-
     console.log(game);
 
     $("#rollButton").click(function () {
-      //game.executeTurn();
+      var roll = game.roll();
+      var turn = game.playerTurn;
+      game.executeTurn(roll, turn);
 
 
     });
